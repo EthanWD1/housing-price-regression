d6a6cf975e33e61a259b775d874ea8a6a941407a
EthanWD1 <subpar.gg@gmail.com>
2025-07-30 11:37:41 -0700

    Added Lasso Regression and Lasso Residual Plot

--- testProject.py
+++ testProject.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
-from sklearn.linear_model import LinearRegression, Ridge
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
 from sklearn.metrics import mean_squared_error, r2_score
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -12,6 +12,7 @@ df = pd.read_csv("cahousing.csv") ### load csv
 #print(df.isnull().sum())
 df['total_bedrooms'] = df['total_bedrooms'].fillna(0)  ### Replace null bedroom counts with 0 bedrooms
 df = pd.get_dummies(df, drop_first=True)  ### qualitative variables to binary categorical variables
+df=df[df['median_house_value']<500000] ### remove all blocks with average house price at $500,000, as dataset caps median price
 X = df.drop('median_house_value', axis=1) ### predictor variables everything aside from house price
 Y = df['median_house_value'] ### predicted price is the estimated variable
 
@@ -30,10 +31,10 @@ predR2 = r2_score(yTest,yPred) ### what percent of variability in data can be ex
 
 print("Cross-Validation R^2 Scores:", cvScores)
 print("Average Cross-Validation R^2 Score:", cvScores.mean())
-print("Model intercept: ", mlrModel.intercept_)
-print("Model coefficients: ", mlrModel.coef_)
-print("Model RMSE: ",predRMSE)
-print("Model R2:", predR2)
+print("Intercept: ", mlrModel.intercept_)
+print("Coefficients: ", mlrModel.coef_)
+print("RMSE: ",predRMSE)
+print("R2:", predR2)
 
 ##################
 
@@ -48,21 +49,51 @@ grid_search.fit(xTrain, yTrain) ### create grid search model with best hyperpara
 
 bestRidge = grid_search.best_estimator_ ###Save the best alpha value for the ridge model
 ridgeCV = cross_val_score(bestRidge,xTrain,yTrain,cv=5,scoring = 'r2') ### cross validation with 5 folds
-gridPred = grid_search.predict(xTest) ### Create predicted values with 20% of the data set aside for testing
+gridPred = bestRidge.predict(xTest) ### Create predicted values with 20% of the data set aside for testing
 
 gridRMSE = np.sqrt(mean_squared_error(yTest,gridPred)) ### find root of MSE by comparing real y test values to predicted values
 gridR2 = r2_score(yTest,gridPred) ### generate r^2 score, percent of variation in y that can be explained by the model
 
-print("Grid best params: ", grid_search.best_params_)
-print("Ridge Cross-Validation R^2 Scores:", ridgeCV)
-print("Average Ridge Cross-Validation R^2 Score:", ridgeCV.mean())
-print("Grid Root MSE: ",gridRMSE)
-print("Grid R2:", gridR2)
+print("Best params: ", bestRidge)
+print("Cross-Validation R^2 Scores:", ridgeCV)
+print("Average Cross-Validation R^2 Score:", ridgeCV.mean())
+print("Root MSE: ",gridRMSE)
+print("R2:", gridR2)
+
+#########
+print("\nLasso Model:")
+lassoModel = Lasso()
+lasso_search = GridSearchCV(lassoModel,paramDict,cv=5,scoring='neg_mean_squared_error')
+lasso_search.fit(xTrain,yTrain)
+bestLasso=lasso_search.best_estimator_
+lasso_CV=cross_val_score(bestLasso,xTrain,yTrain,cv=5,scoring='r2')
+lassoPred=bestLasso.predict(xTest)
+lassoRMSE = np.sqrt(mean_squared_error(yTest,lassoPred))
+lassoR2 = r2_score(yTest,lassoPred)
+
+print("Best params: ", bestLasso)
+print("Cross-Validation R^2 Scores:", lasso_CV)
+print("Average Cross-Validation R^2 Score:", lasso_CV.mean())
+print("Root MSE: ",lassoRMSE)
+print("R2:", lassoR2)
+print("Coefficients: ", bestLasso.coef_)
+
+plt.figure(figsize=(12, 5))
 
 ridgeResid=gridPred-yTest
+plt.subplot(1, 2, 1)
 plt.scatter(yTest, ridgeResid)
 plt.xlabel("Actual block median value")
 plt.ylabel("Ridge model residual")
 plt.title("Ridge regression residual plot")
 plt.axhline(y=0,color='r')
+
+lassoResid = lassoPred - yTest
+plt.subplot(1, 2, 2)
+plt.scatter(yTest, lassoResid)
+plt.xlabel("Actual block median value")
+plt.ylabel("Lasso model residual")
+plt.title("Lasso regression residual plot")
+plt.axhline(y=0, color='r')
+plt.tight_layout()
 plt.show()
